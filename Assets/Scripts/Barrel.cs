80caea6b79dd8a61ec48a9370ec2942c15d2b74a
StarPatric <unigeorge98@gmail.com>
2019-07-20 13:13:47 +1200

    BArrel Explosion

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : IPingable
+{
+    public GameObject Explosion;    //For the Explosion Sprite     
+
+    CharacterController controller;
+
+    public override void Ping()
+    {
+        controller = GetComponent<CharacterController>();
+
+        if (controller.detectCollisions == true)
+        {
+            Explode((Collider2D)GetComponent<Collider2D>());
+        }
+
+    }
+
+    private void Explode(Collider2D Self)
+    {
+        GameObject Explode = Instantiate(Explosion) as GameObject;
+        Explode.transform.position = transform.position;
+        Destroy(Self.gameObject);
+        this.gameObject.SetActive(false);
+    }
+}
