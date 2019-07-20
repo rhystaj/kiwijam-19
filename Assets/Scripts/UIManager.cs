bad71d8d8f93485230def126ffb19a3b4e10c8f6
Rhys Angelo-Jones <rhystaj@gmail.com>
2019-07-21 10:08:18 +1200

    Game over state.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] Text failStateText;
+
+    public void ShowFailStateScreen()
+    {
+        failStateText.enabled = true;
+    }
+
+    public void HideFailStateScreen()
+    {
+        failStateText.enabled = false;
+    }
+
+
+}
