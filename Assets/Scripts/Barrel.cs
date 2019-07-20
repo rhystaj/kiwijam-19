8a81a57ed41da3ebec9761d86a257eb5433c6ab5
StarPatric <unigeorge98@gmail.com>
2019-07-20 18:03:55 +1200

    FIxed resset

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -24,6 +24,7 @@ private bool exploded = false;
         if (Preprimed) explodeOnTurn = 0;
         Explosion = this.GetComponent<Animator>();
         ExplosionSound = this.GetComponent<AudioSource>();
+        Sprite = this.GetComponent<SpriteRenderer>();
     }
 
 
