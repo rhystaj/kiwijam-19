6ac09fc6afd30a5364939ca5485bcd499fe52e4c
StarPatric <unigeorge98@gmail.com>
2019-07-21 15:58:40 +1200

    Stuff

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLevel : MonoBehaviour
+{
+
+    public string Level;
+
+    private void OnMouseDown()
+    {
+        Application.LoadLevel(Level);
+    }
+}
